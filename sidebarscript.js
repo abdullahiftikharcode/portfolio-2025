a591c9b7e2e945b73e9c024c8d249b639e77722f
Abdullah Iftikhar <ai868419@gmail.com>
2025-05-03 07:52:17 +0500

    "final animation commit"

--- sidebarscript.js
+++ sidebarscript.js
@@ -1,4 +1,3 @@
-// Attach a click event listener to the element with class "menu-toggle" to open and close menu
 document.querySelector(".menu-toggle").addEventListener("click", function() {
     document.getElementById("sidebar").classList.toggle("active");
     document.querySelector(".content").classList.toggle("active");
